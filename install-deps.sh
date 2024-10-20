5632c619e1b512dfbf3026de39c3ef7a5c745793
Ryan Stocks <ryan.stocks00@gmail.com>
2024-10-20 15:04:39 +1100

    Install opengl dependencies

--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y libgdal-dev libpdal-dev libopencv-dev cmake libomp-dev rpm qt6-base-dev
+sudo apt install -y libgdal-dev libpdal-dev libopencv-dev cmake libomp-dev rpm qt6-base-dev libglx-dev libgl1-mesa-dev
