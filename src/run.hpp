b1837272cac66d40757a88df722c6d2aea507a0c
Ryan Stocks <ryan.stocks00@gmail.com>
2024-10-27 13:40:52 +1100

    Correct struct/class

--- src/run.hpp
+++ src/run.hpp
@@ -4,7 +4,7 @@
 
 #include "utilities/filesystem.hpp"
 
-class Config;
+struct Config;
 class ProgressTracker;
 
 void run_with_config(const Config& config, const std::vector<fs::path>& additional_las_files,
