01fb6619d1c637818875ad909e4ded40e4d4db9b
Ryan Stocks <ryan.stocks00@gmail.com>
2025-01-22 10:23:15 +1100

    Fix install deps formatting

--- install-deps.sh
+++ install-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y libgdal-dev libopencv-dev cmake libomp-dev rpm qt6-base-dev
+sudo apt install -y libgdal-dev libopencv-dev cmake libomp-dev rpm qt6-base-dev \
 libglx-dev libgl1-mesa-dev qt6-image-formats-plugins libqt6svg6-dev
