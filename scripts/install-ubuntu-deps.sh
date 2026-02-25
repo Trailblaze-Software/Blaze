e19b6d81b71c15b1d3efd79243c90c70fc8409cb
Ryan Stocks <ryan.stocks00@gmail.com>
2026-02-26 00:24:19 +1100

    Better apt pkg caching for CI

--- scripts/install-ubuntu-deps.sh
+++ scripts/install-ubuntu-deps.sh
@@ -7,6 +7,6 @@ else
 	SUDO=""
 fi
 
-$SUDO apt update
-$SUDO apt install -y libgdal-dev libopencv-dev cmake libomp-dev rpm qt6-base-dev \
-libglx-dev libgl1-mesa-dev qt6-image-formats-plugins libqt6svg6-dev ccache mold ninja-build
+$SUDO apt-get update
+$SUDO apt-get install -y --no-install-recommends libgdal-dev libopencv-dev cmake libomp-dev rpm qt6-base-dev \
+	libglx-dev libgl1-mesa-dev qt6-image-formats-plugins libqt6svg6-dev ccache mold ninja-build
